f88a3f5199ac3aa2ec33a7b235c2dead73775a5e
chemisky <yves.chemisky@gmail.com>
2016-05-31 14:54:54 -0400

    Added types for tests

--- test/Tarma2numpy.cpp
+++ test/Tarma2numpy.cpp
@@ -23,6 +23,7 @@
 #include <boost/python.hpp>
 #include <boost/numpy.hpp>
 #include <arma2numpy/numpy_arma.hpp>
+#include <arma2numpy/list_vector.hpp>
 
 namespace bp = boost::python;
 namespace bn = boost::numpy;
@@ -31,15 +32,41 @@ using namespace arma;
 
 namespace arma2numpy {
 
-bn::ndarray test_vec(bn::ndarray const &y) {
+bn::ndarray test_vec_int(bn::ndarray const &y) {
+    Col<int> v = array2Col_int(y);
+    return Col_int2array(v);
+}
+
+bn::ndarray test_mat_int(bn::ndarray const &y) {
+    Mat<int> m = array2Mat_int(y);
+    return Mat_int2array(m);
+}
+    
+bn::ndarray test_vec_double(bn::ndarray const &y) {
     vec v = array2vec(y);
     return vec2array(v);
 }
 
-bn::ndarray test_mat(bn::ndarray const &y) {
+bn::ndarray test_mat_double(bn::ndarray const &y) {
     mat m = array2mat(y);
     return mat2array(m);
 }
+    
+bp::list test_vector_list_double(bp::list const &y) {
+ std::vector<double> v = py_list_to_std_vector_double(y);
+ return std_vector_to_py_list_double(v);
+}
+    
+bp::list test_vector_list_int(bp::list const &y) {
+    std::vector<int> v = py_list_to_std_vector_int(y);
+    return std_vector_to_py_list_int(v);
+}
 
+bp::list test_vector_list_string(bp::list const &y) {
+    std::vector<std::string> v = py_list_to_std_vector_string(y);
+    return std_vector_to_py_list_string(v);
+}
+
+    
 } //end of namespace arma2numpy
 
