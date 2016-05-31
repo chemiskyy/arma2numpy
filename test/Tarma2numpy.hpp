83f66d6cb5f3a68e0b46cfdde79ab00729448e4d
chemisky <yves.chemisky@gmail.com>
2016-05-31 14:55:12 -0400

    Added types to test

--- test/Tarma2numpy.hpp
+++ test/Tarma2numpy.hpp
@@ -24,8 +24,19 @@
 
 namespace arma2numpy {
 
-boost::numpy::ndarray test_vec(boost::numpy::ndarray const &y);
+boost::numpy::ndarray test_vec_int(boost::numpy::ndarray const &y);
 
-boost::numpy::ndarray test_mat(boost::numpy::ndarray const &y);
+boost::numpy::ndarray test_mat_int(boost::numpy::ndarray const &y);
+    
+boost::numpy::ndarray test_vec_double(boost::numpy::ndarray const &y);
 
+boost::numpy::ndarray test_mat_double(boost::numpy::ndarray const &y);
+
+boost::python::list test_vector_list_double(boost::python::list const &y);
+    
+boost::python::list test_vector_list_int(boost::python::list const &y);
+
+boost::python::list test_vector_list_string(boost::python::list const &y);
+
+    
 } //end of namespace arma2numpy
