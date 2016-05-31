ac44b24c1b69f7f620157594db79ab3f6371b43b
chemisky <yves.chemisky@gmail.com>
2016-05-31 14:54:18 -0400

    test function for vector - list converters

--- test/boostpython_arma_wrapper.cpp
+++ test/boostpython_arma_wrapper.cpp
@@ -3,6 +3,7 @@
 #include <boost/python.hpp>
 #include <boost/numpy.hpp>
 #include <arma2numpy/numpy_arma.hpp>
+#include <arma2numpy/list_vector.hpp>
 #include "Tarma2numpy.hpp"
 
 namespace bp = boost::python;
@@ -17,6 +18,11 @@ BOOST_PYTHON_MODULE(Tarma2numpy) {
     bn::initialize();
     
     // Register the from-python converters
-    bp::def("test_vec", test_vec);
-    bp::def("test_mat", test_mat);
+    bp::def("test_vec_int", test_vec_int);
+    bp::def("test_mat_int", test_mat_int);
+    bp::def("test_vec_double", test_vec_double);
+    bp::def("test_mat_double", test_mat_double);
+    bp::def("test_vector_list_double", test_vector_list_double);
+    bp::def("test_vector_list_int", test_vector_list_int);
+    bp::def("test_vector_list_string", test_vector_list_string);
 }
